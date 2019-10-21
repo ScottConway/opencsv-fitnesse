293429b153d50127adcd7b5e0e0e389d8580a0ae
Conway, Scott <Scott.Conway@sabre.com>
2019-10-21 13:52:04 -0500

    Got first wiki test to run - not pass because there is no true test inside, but it is running.

--- fitnesse/startFitnesse.sh
+++ fitnesse/startFitnesse.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cp ../target/opencsv-fitnesse*.jar ./lib/opencsv-fitnesse.jar
+chmod a+rwx ./lib/*.jar
 
 java -jar fitnesse-standalone.jar -p 10000
 
--- src/main/java/com/opencsv/fitnesse/CsvParserSetupFixture.java
+++ src/main/java/com/opencsv/fitnesse/CsvParserSetupFixture.java
@@ -6,11 +6,15 @@ import fitlibrary.SetUpFixture;
 public class CsvParserSetupFixture extends SetUpFixture {
     private ICSVParser parserList;
 
+    public CsvParserSetupFixture() {
+    }
+
     public void separatorQuoteEscapeStrictQuotesIgnoreLeadingWhiteSpaceIgnoreQuotationsNullFieldIndicator(char separator,
                                                                                                           char quote, char escape,
+                                                                                                          boolean strictQuotes,
                                                                                                           boolean ignoreLeadingWhiteSpace,
                                                                                                           boolean ignoreQuotations,
                                                                                                           String nullFieldIndicator) {
-
+    System.out.println("Hello world");
     }
 }
